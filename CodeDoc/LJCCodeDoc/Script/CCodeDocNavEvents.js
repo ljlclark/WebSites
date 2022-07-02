fb85270e391c480aab2dcd519765416d1b24db64
ljlclark <35316538+ljlclark@users.noreply.github.com>
2022-07-03 05:44:59 +0800

    Conform to standards

--- CodeDoc/LJCCodeDoc/Script/CCodeDocNavEvents.js
+++ CodeDoc/LJCCodeDoc/Script/CCodeDocNavEvents.js
@@ -4,184 +4,184 @@
 
 class CNavEvents
 {
-	// Initializes an object instance with the provided values.
-	constructor(contentFrame)
-	{
-		this.ContentFrame = contentFrame;
-		this.PrevNavItem = null;
-		this.NavItems = new CodeDocNavItems();
-		this.CreateNavItems();
-	}
+  // Initializes an object instance with the provided values.
+  constructor(contentFrame)
+  {
+    this.ContentFrame = contentFrame;
+    this.PrevNavItem = null;
+    this.NavItems = new CodeDocNavItems();
+    this.CreateNavItems();
+  }
 
-	// Adds the HTML event handlers.
-	AddEvents()
-	{
-		// Document Event Handlers.
-		document.addEventListener("click", this.DocumentClick.bind(this));
-	}
+  // Adds the HTML event handlers.
+  AddEvents()
+  {
+    // Document Event Handlers.
+    document.addEventListener("click", this.DocumentClick.bind(this));
+  }
 
-	// Document "click" handler method.
-	// event - The Target event.
-	DocumentClick(event)
-	{
-		let srcElement = event.target;
-		if ("navGroup" == srcElement.className
-			|| "navItem" == srcElement.className)
-		{
-			let navItem = this.NavItems.SearchName(srcElement.id);
-			if (navItem != null)
-			{
-				if (this.ContentFrame != null)
-				{
-					this.ContentFrame.src = navItem.URL;
-				}
-			}
+  // Document "click" handler method.
+  // event - The Target event.
+  DocumentClick(event)
+  {
+    const srcElement = event.target;
+    if ("navGroup" === srcElement.className
+      || "navItem" === srcElement.className)
+    {
+      const navItem = this.NavItems.SearchName(srcElement.id);
+      if (navItem != null)
+      {
+        if (this.ContentFrame != null)
+        {
+          this.ContentFrame.src = navItem.URL;
+        }
+      }
 
-			if (this.PrevNavItem != null)
-			{
-				this.PrevNavItem.style.backgroundColor = "";
-			}
-			this.PrevNavItem = srcElement;
-			srcElement.style.backgroundColor = "#d4dfff";
-		}
-	}
+      if (this.PrevNavItem != null)
+      {
+        this.PrevNavItem.style.backgroundColor = "";
+      }
+      this.PrevNavItem = srcElement;
+      srcElement.style.backgroundColor = "#d4dfff";
+    }
+  }
 
-	// Creates the NavItem entries.
-	CreateNavItems()
-	{
-		this.NavItems.Add("Projects", "CodeDoc.html");
-		this.NavItems.Add("Build", "SystemBuild.html");
+  // Creates the NavItem entries.
+  CreateNavItems()
+  {
+    this.NavItems.Add("Projects", "CodeDoc.html");
+    this.NavItems.Add("Build", "SystemBuild.html");
 
-		// * Common Libraries
-		// - .NET Code
-		this.NavItems.Add("NetCommon", "HTML/LJCNetCommon/LJCNetCommon.html");
-		this.NavItems.Add("DBScript", "HTML/LJCExecuteScript/LJCExecuteScript.html");
-		this.NavItems.Add("Parser", "HTML/LJCAddressParserLib/LJCAddressParserLib.html");
+    // * Common Libraries
+    // - .NET Code
+    this.NavItems.Add("NetCommon", "HTML/LJCNetCommon/LJCNetCommon.html");
+    this.NavItems.Add("DBScript", "HTML/LJCExecuteScript/LJCExecuteScript.html");
+    this.NavItems.Add("Parser", "HTML/LJCAddressParserLib/LJCAddressParserLib.html");
 
-		// - WinForms
-		this.NavItems.Add("Common", "HTML/LJCWinFormCommon/LJCWinFormCommon.html");
-		this.NavItems.Add("Controls", "HTML/LJCWinFormControls/LJCWinFormControls.html");
-		this.NavItems.Add("GridLib", "HTML/LJCGridDataLib/LJCGridDataLib.html");
+    // - WinForms
+    this.NavItems.Add("Common", "HTML/LJCWinFormCommon/LJCWinFormCommon.html");
+    this.NavItems.Add("Controls", "HTML/LJCWinFormControls/LJCWinFormControls.html");
+    this.NavItems.Add("GridLib", "HTML/LJCGridDataLib/LJCGridDataLib.html");
 
-		// - Text DataReader
-		this.NavItems.Add("Reader", "HTML/LJCTextDataReaderLib/LJCTextDataReaderLib.html");
-		this.NavItems.Add("TestCon", "HTML/LJCTestConsole/LJCTestConsole.html");
+    // - Text DataReader
+    this.NavItems.Add("Reader", "HTML/LJCTextDataReaderLib/LJCTextDataReaderLib.html");
+    this.NavItems.Add("TestCon", "HTML/LJCTestConsole/LJCTestConsole.html");
 
-		// - DataDetail
-		this.NavItems.Add("Dtl", "HTML/DataDetail/DataDetail.html");
-		this.NavItems.Add("DtlLib", "HTML/LJCDataDetailLib/LJCDataDetailLib.html");
-		this.NavItems.Add("DtlCon", "HTML/LJCDataDetailConsole/LJCDataDetailConsole.html");
-		this.NavItems.Add("TestData", "HTML/LJCTestDataLib/LJCTestDataLib.html");
+    // - DataDetail
+    this.NavItems.Add("Dtl", "HTML/DataDetail/DataDetail.html");
+    this.NavItems.Add("DtlLib", "HTML/LJCDataDetailLib/LJCDataDetailLib.html");
+    this.NavItems.Add("DtlCon", "HTML/LJCDataDetailConsole/LJCDataDetailConsole.html");
+    this.NavItems.Add("TestData", "HTML/LJCTestDataLib/LJCTestDataLib.html");
 
-		// * Data Libraries
-		// - Data Access
-		this.NavItems.Add("Overview", "DataOverview.html");
-		this.NavItems.Add("Data", "HTML/LJCDataAccess/LJCDataAccess.html");
-		this.NavItems.Add("DBData", "HTML/LJCDBDataAccessLib/LJCDBDataAccessLib.html");
-		this.NavItems.Add("DataTest", "HTML/LJCDataAccessTest/LJCDataAccessTest.html");
-		this.NavItems.Add("Config", "HTML/LJCDataAccessConfig/LJCDataAccessConfig.html");
-		this.NavItems.Add("ConfigCon", "HTML/ConfigTestConsole/ConfigTestConsole.html");
+    // * Data Libraries
+    // - Data Access
+    this.NavItems.Add("Overview", "DataOverview.html");
+    this.NavItems.Add("Data", "HTML/LJCDataAccess/LJCDataAccess.html");
+    this.NavItems.Add("DBData", "HTML/LJCDBDataAccessLib/LJCDBDataAccessLib.html");
+    this.NavItems.Add("DataTest", "HTML/LJCDataAccessTest/LJCDataAccessTest.html");
+    this.NavItems.Add("Config", "HTML/LJCDataAccessConfig/LJCDataAccessConfig.html");
+    this.NavItems.Add("ConfigCon", "HTML/ConfigTestConsole/ConfigTestConsole.html");
 
-		// - Client
-		this.NavItems.Add("Client", "HTML/LJCDBClientLib/LJCDBClientLib.html");
-		this.NavItems.Add("Message", "HTML/LJCDBMessage/LJCDBMessage.html");
-		this.NavItems.Add("Key", "HTML/ForeignKeyManagerTest/ForeignKeyManagerTest.html");
-		this.NavItems.Add("SQLUtil", "HTML/LJCSQLUtilLib/LJCSQLUtilLib.html");
-		this.NavItems.Add("SQLUtilDAL", "HTML/LJCSQLUtilLibDAL/LJCSQLUtilLibDAL.html");
+    // - Client
+    this.NavItems.Add("Client", "HTML/LJCDBClientLib/LJCDBClientLib.html");
+    this.NavItems.Add("Message", "HTML/LJCDBMessage/LJCDBMessage.html");
+    this.NavItems.Add("Key", "HTML/ForeignKeyManagerTest/ForeignKeyManagerTest.html");
+    this.NavItems.Add("SQLUtil", "HTML/LJCSQLUtilLib/LJCSQLUtilLib.html");
+    this.NavItems.Add("SQLUtilDAL", "HTML/LJCSQLUtilLibDAL/LJCSQLUtilLibDAL.html");
 
-		// - Server
-		this.NavItems.Add("Host", "HTML/LJCDBServiceHost/LJCDBServiceHost.html");
-		this.NavItems.Add("HostCon", "HTML/LJCDBServiceConsoleHost/LJCDBServiceConsoleHost.html");
-		this.NavItems.Add("DBService", "HTML/LJCDBServiceLib/LJCDBServiceLib.html");
+    // - Server
+    this.NavItems.Add("Host", "HTML/LJCDBServiceHost/LJCDBServiceHost.html");
+    this.NavItems.Add("HostCon", "HTML/LJCDBServiceConsoleHost/LJCDBServiceConsoleHost.html");
+    this.NavItems.Add("DBService", "HTML/LJCDBServiceLib/LJCDBServiceLib.html");
 
-		// - Person DAL Test
-		this.NavItems.Add("PDALTest", "HTML/PersonDALTest/PersonDALTest.html");
-		this.NavItems.Add("PDAL", "HTML/PersonDAL/PersonDAL.html");
+    // - Person DAL Test
+    this.NavItems.Add("PDALTest", "HTML/PersonDALTest/PersonDALTest.html");
+    this.NavItems.Add("PDAL", "HTML/PersonDAL/PersonDAL.html");
 
-		// * Code Generator Utility
-		// - Code Generator
-		this.NavItems.Add("GenText", "HTML/LJCGenText/LJCGenText.html");
-		this.NavItems.Add("GenLib", "HTML/LJCGenTextLib/LJCGenTextLib.html");
+    // * Code Generator Utility
+    // - Code Generator
+    this.NavItems.Add("GenText", "HTML/LJCGenText/LJCGenText.html");
+    this.NavItems.Add("GenLib", "HTML/LJCGenTextLib/LJCGenTextLib.html");
 
-		// - Code Generation Editor
-		this.NavItems.Add("GenEdit", "HTML/LJCGenTextEdit/LJCGenTextEdit.html");
-		this.NavItems.Add("GenTable", "HTML/LJCGenTableCode/LJCGenTableCode.html");
+    // - Code Generation Editor
+    this.NavItems.Add("GenEdit", "HTML/LJCGenTextEdit/LJCGenTextEdit.html");
+    this.NavItems.Add("GenTable", "HTML/LJCGenTableCode/LJCGenTableCode.html");
 
-		// * HTML Documentation Generator
-		// - Page Generation
-		this.NavItems.Add("DocGen", "HTML/LJCDocGen/LJCDocGen.html");
-		this.NavItems.Add("GenPage", "HTML/LJCGenPageGen/LJCGenPageGen.html");
-		this.NavItems.Add("DocLib", "HTML/LJCDocGenLib/LJCDocGenLib.html");
+    // * HTML Documentation Generator
+    // - Page Generation
+    this.NavItems.Add("DocGen", "HTML/LJCDocGen/LJCDocGen.html");
+    this.NavItems.Add("GenPage", "HTML/LJCGenPageGen/LJCGenPageGen.html");
+    this.NavItems.Add("DocLib", "HTML/LJCDocGenLib/LJCDocGenLib.html");
 
-		// - Deserialization Objects
-		this.NavItems.Add("DocDAL", "HTML/LJCDocLibDAL/LJCDocLibDAL.html");
-		this.NavItems.Add("DocObj", "HTML/LJCDocObjLib/LJCDocObjLib.html");
-		this.NavItems.Add("DocXML", "HTML/LJCDocXMLObjLib/LJCDocXMLObjLib.html");
+    // - Deserialization Objects
+    this.NavItems.Add("DocDAL", "HTML/LJCDocLibDAL/LJCDocLibDAL.html");
+    this.NavItems.Add("DocObj", "HTML/LJCDocObjLib/LJCDocObjLib.html");
+    this.NavItems.Add("DocXML", "HTML/LJCDocXMLObjLib/LJCDocXMLObjLib.html");
 
-		// - Group Editor
-		this.NavItems.Add("DocEdit", "HTML/LJCDocGroupEditor/LJCDocGroupEditor.html");
+    // - Group Editor
+    this.NavItems.Add("DocEdit", "HTML/LJCDocGroupEditor/LJCDocGroupEditor.html");
 
-		// * Data Transform Projects
-		this.NavItems.Add("DTMgr", "HTML/LJCTransformManager/LJCTransformManager.html");
-		this.NavItems.Add("DTProcess", "HTML/LJCDataTransformProcess/LJCDataTransformProcess.html");
-		this.NavItems.Add("CommonMod", "HTML/LJCCommonModuleLib/LJCCommonModuleLib.html");
-		this.NavItems.Add("DTDAL", "HTML/LJCDataTransformDAL/LJCDataTransformDAL.html");
-		this.NavItems.Add("DTTest", "HTML/TransformServiceTest/TransformServiceTest.html");
+    // * Data Transform Projects
+    this.NavItems.Add("DTMgr", "HTML/LJCTransformManager/LJCTransformManager.html");
+    this.NavItems.Add("DTProcess", "HTML/LJCDataTransformProcess/LJCDataTransformProcess.html");
+    this.NavItems.Add("CommonMod", "HTML/LJCCommonModuleLib/LJCCommonModuleLib.html");
+    this.NavItems.Add("DTDAL", "HTML/LJCDataTransformDAL/LJCDataTransformDAL.html");
+    this.NavItems.Add("DTTest", "HTML/TransformServiceTest/TransformServiceTest.html");
 
-		// * CVR Manager
-		this.NavItems.Add("CVMgr", "HTML/CVRManager/CVRManager.html");
-		this.NavItems.Add("CVDAL", "HTML/CVRDAL/CVRDAL.html");
+    // * CVR Manager
+    this.NavItems.Add("CVMgr", "HTML/CVRManager/CVRManager.html");
+    this.NavItems.Add("CVDAL", "HTML/CVRDAL/CVRDAL.html");
 
-		// * Sales Manager
-		this.NavItems.Add("SalesMgr", "HTML/LJCSalesManager/LJCSalesManager.html");
-		this.NavItems.Add("SalesDAL", "HTML/LJCSalesDAL/LJCSalesDAL.html");
+    // * Sales Manager
+    this.NavItems.Add("SalesMgr", "HTML/LJCSalesManager/LJCSalesManager.html");
+    this.NavItems.Add("SalesDAL", "HTML/LJCSalesDAL/LJCSalesDAL.html");
 
-		// * Unit Measure
-		this.NavItems.Add("Unit", "HTML/LJCUnitMeasure/LJCUnitMeasure.html");
-		this.NavItems.Add("UnitDAL", "HTML/LJCUnitMeasureDAL/LJCUnitMeasureDAL.html");
+    // * Unit Measure
+    this.NavItems.Add("Unit", "HTML/LJCUnitMeasure/LJCUnitMeasure.html");
+    this.NavItems.Add("UnitDAL", "HTML/LJCUnitMeasureDAL/LJCUnitMeasureDAL.html");
 
-		// * Application Manager
-		this.NavItems.Add("App", "HTML/LJCAppManager/LJCAppManager.html");
-		this.NavItems.Add("AppDAL", "HTML/LJCAppManagerDAL/LJCAppManagerDAL.html");
+    // * Application Manager
+    this.NavItems.Add("App", "HTML/LJCAppManager/LJCAppManager.html");
+    this.NavItems.Add("AppDAL", "HTML/LJCAppManagerDAL/LJCAppManagerDAL.html");
 
-		// * Facility Manager
-		this.NavItems.Add("FacMgr", "HTML/LJCFacilityManager/LJCFacilityManager.html");
-		this.NavItems.Add("FacDAL", "HTML/LJCFacilityManagerDAL/LJCFacilityManagerDAL.html");
-		this.NavItems.Add("FacForm", "HTML/LJCFacilityForm/LJCFacilityForm.html");
-		this.NavItems.Add("ModHost", "HTML/ModuleHost/ModuleHost.html");
+    // * Facility Manager
+    this.NavItems.Add("FacMgr", "HTML/LJCFacilityManager/LJCFacilityManager.html");
+    this.NavItems.Add("FacDAL", "HTML/LJCFacilityManagerDAL/LJCFacilityManagerDAL.html");
+    this.NavItems.Add("FacForm", "HTML/LJCFacilityForm/LJCFacilityForm.html");
+    this.NavItems.Add("ModHost", "HTML/ModuleHost/ModuleHost.html");
 
-		// * Facility Manager Setup
-		this.NavItems.Add("FacSetup", "HTML/LJCFacilityManagerSetup/LJCFacilityManagerSetup.html");
+    // * Facility Manager Setup
+    this.NavItems.Add("FacSetup", "HTML/LJCFacilityManagerSetup/LJCFacilityManagerSetup.html");
 
-		// * Region Manager
-		this.NavItems.Add("RegMgr", "HTML/LJCRegionManager/LJCRegionManager.html");
-		this.NavItems.Add("RegDAL", "HTML/LJCRegionDAL/LJCRegionDAL.html");
-		this.NavItems.Add("RegForm", "HTML/LJCRegionForm/LJCRegionForm.html");
+    // * Region Manager
+    this.NavItems.Add("RegMgr", "HTML/LJCRegionManager/LJCRegionManager.html");
+    this.NavItems.Add("RegDAL", "HTML/LJCRegionDAL/LJCRegionDAL.html");
+    this.NavItems.Add("RegForm", "HTML/LJCRegionForm/LJCRegionForm.html");
 
-		// * DocApp Manager
-		this.NavItems.Add("DocAppMgr", "HTML/LJCDocAppManager/LJCDocAppManager.html");
-		this.NavItems.Add("DocAppDAL", "HTML/LJCDocAppManagerDAL/LJCDocAppManagerDAL.html");
+    // * DocApp Manager
+    this.NavItems.Add("DocAppMgr", "HTML/LJCDocAppManager/LJCDocAppManager.html");
+    this.NavItems.Add("DocAppDAL", "HTML/LJCDocAppManagerDAL/LJCDocAppManagerDAL.html");
 
-		// * DataView
-		// - DataView Data Access Layer
-		this.NavItems.Add("ViewDAL", "HTML/LJCDBViewDAL/LJCDBViewDAL.html");
-		this.NavItems.Add("ConApp2", "HTML/ConsoleApp2/ConsoleApp2.html");
+    // * DataView
+    // - DataView Data Access Layer
+    this.NavItems.Add("ViewDAL", "HTML/LJCDBViewDAL/LJCDBViewDAL.html");
+    this.NavItems.Add("ConApp2", "HTML/ConsoleApp2/ConsoleApp2.html");
 
-		// - DataView Builder
-		this.NavItems.Add("ViewBuild", "HTML/LJCViewBuilder/LJCViewBuilder.html");
+    // - DataView Builder
+    this.NavItems.Add("ViewBuild", "HTML/LJCViewBuilder/LJCViewBuilder.html");
 
-		// - DataView Editor
-		this.NavItems.Add("ViewEdit", "HTML/LJCViewEditor/LJCViewEditor.html");
-		this.NavItems.Add("ViewEditDAL", "HTML/LJCViewEditorDAL/LJCViewEditorDAL.html");
+    // - DataView Editor
+    this.NavItems.Add("ViewEdit", "HTML/LJCViewEditor/LJCViewEditor.html");
+    this.NavItems.Add("ViewEditDAL", "HTML/LJCViewEditorDAL/LJCViewEditorDAL.html");
 
-		// * Code Line Counter and Text Finder
-		this.NavItems.Add("LineCount", "HTML/LJCCodeLineCounter/LJCCodeLineCounter.html");
+    // * Code Line Counter and Text Finder
+    this.NavItems.Add("LineCount", "HTML/LJCCodeLineCounter/LJCCodeLineCounter.html");
 
-		// * Text Invasion Typing Game
-		this.NavItems.Add("TextInvade", "HTML/LJCTextInvasion/LJCTextInvasion.html");
+    // * Text Invasion Typing Game
+    this.NavItems.Add("TextInvade", "HTML/LJCTextInvasion/LJCTextInvasion.html");
 
-		// * Pagination Testing
-		this.NavItems.Add("DataList", "HTML/LJCDataPageList/LJCDataPageList.html");
-		this.NavItems.Add("TextList", "HTML/LJCTextPageList/LJCTextPageList.html");
-	}
+    // * Pagination Testing
+    this.NavItems.Add("DataList", "HTML/LJCDataPageList/LJCDataPageList.html");
+    this.NavItems.Add("TextList", "HTML/LJCTextPageList/LJCTextPageList.html");
+  }
 }
--- CodeDoc/LJCCodeDocRelease/Script/CodeDocNavItems.js
+++ CodeDoc/LJCCodeDocRelease/Script/CodeDocNavItems.js
@@ -6,112 +6,112 @@
 /// <summary>Contains CityDetail event handlers.</summary>
 class CodeDocNavItem
 {
-	// Initializes an object instance with the provided values.
-	constructor(name, url)
-	{
-		this.Name = name;
-		this.URL = url;
-	}
+  // Initializes an object instance with the provided values.
+  constructor(name, url)
+  {
+    this.Name = name;
+    this.URL = url;
+  }
 
-	// Clones this object.
-	Clone()
-	{
-		return new CodeDocNavItem(this.Name, this.URL);
-	}
+  // Clones this object.
+  Clone()
+  {
+    return new CodeDocNavItem(this.Name, this.URL);
+  }
 }
 
 // ***************
 /// <summary>Represents a collection of NavItem objects.</summary>
 class CodeDocNavItems
 {
-	// Initializes an object instance.
-	constructor()
-	{
-		this.Items = [];
-	}
+  // Initializes an object instance.
+  constructor()
+  {
+    this.Items = [];
+  }
 
-	// Creates and adds a strong typed item to the collection.
-	// Returns the added item.
-	Add(name, url)
-	{
-		let item = new CodeDocNavItem(name, url);
-		this.Items.push(item);
-		return item;
-	}
+  // Creates and adds a strong typed item to the collection.
+  // Returns the added item.
+  Add(name, url)
+  {
+    const item = new CodeDocNavItem(name, url);
+    this.Items.push(item);
+    return item;
+  }
 
-	// Clones this object.
-	Clone()
-	{
-		let retValue = new CodeDocNavItems();
-		let len = this.Items.length;
-		for (let index = 0; index < len; Index++)
-		{
-			let item = this.Items[index];
-			let newItem = item.Clone();
-			retValue.Items.push(newItem);
-		}
-		return retValue;
-	}
+  // Clones this object.
+  Clone()
+  {
+    const retValue = new CodeDocNavItems();
+    const len = this.Items.length;
+    for (const index = 0; index < len; Index++)
+    {
+      const item = this.Items[index];
+      const newItem = item.Clone();
+      retValue.Items.push(newItem);
+    }
+    return retValue;
+  }
 
-	// 
-	Get(index)
-	{
-		return this.Items[index];
-	}
+  // 
+  Get(index)
+  {
+    return this.Items[index];
+  }
 
-	// Returns an item from the collection by Name.
-	FindName(name)
-	{
-		let retValue = null;
+  // Returns an item from the collection by Name.
+  FindName(name)
+  {
+    const retValue = null;
 
-		if (Common.HasValue(name))
-		{
-			let len = this.Items.length;
-			for (let index = 0; index < len; index++)
-			{
-				let item = this.Items[index];
-				if (item.Name != null
-					&& 0 == item.Name.localeCompare(name))
-				{
-					retValue = item;
-					break;
-				}
-			}
-		}
-		return retValue;
-	}
+    if (Common.HasValue(name))
+    {
+      const len = this.Items.length;
+      for (const index = 0; index < len; index++)
+      {
+        const item = this.Items[index];
+        if (item.Name != null
+          && 0 === item.Name.localeCompare(name))
+        {
+          retValue = item;
+          break;
+        }
+      }
+    }
+    return retValue;
+  }
 
-	// Returns an item from the collection by Name.
-	SearchName(name, threshold = 10)
-	{
-		var retValue;
+  // Returns an item from the collection by Name.
+  SearchName(name, threshold = 10)
+  {
+    let retValue;
 
-		if (Common.HasValue(name))
-		{
-			if (this.Items.length < threshold)
-			{
-				retValue = this.FindName(name);
-			}
-			else
-			{
-				let index = Common.BinarySearch(this.Items,
-					function (compare, compareTo)
-					{
-						// Sort
-						return compare.Name.localeCompare(compareTo.Name);
-					},
-					function (item)
-					{
-						// Compare
-						return item.Name.localeCompare(name);
-					}
-				);
-				if (index > -1)
-				{
-					retValue = this.Items[index];
-				}
-			}
-		}
-		return retValue;
-	}
+    if (Common.HasValue(name))
+    {
+      if (this.Items.length < threshold)
+      {
+        retValue = this.FindName(name);
+      }
+      else
+      {
+        const index = Common.BinarySearch(this.Items,
+          function (compare, compareTo)
+          {
+            // Sort
+            return compare.Name.localeCompare(compareTo.Name);
+          },
+          function (item)
+          {
+            // Compare
+            return item.Name.localeCompare(name);
+          }
+        );
+        if (index > -1)
+        {
+          retValue = this.Items[index];
+        }
+      }
+    }
+    return retValue;
+  }
 }
